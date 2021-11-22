2e461d36622629c212d9ec8915a5a147e263a5b3
Balabi94 <orfejorfeev@gmail.com>
2021-11-23 03:07:04 +0600

    В контекст шаблона передается содержимое post запроса

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,4 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
 
 from lists.views import home_page
 
@@ -8,8 +6,13 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     ''' тест домашней страницы '''
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         ''' test: домашняя страница возвращает правильный html '''
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_can_save_a_POST_request(self):
+        ''' тест: можно сохранить post-запрос '''
+        response = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
