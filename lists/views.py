3828e70e018e07a85a6796b047435069138ffd3f
Balabi94 <orfejorfeev@gmail.com>
2021-11-24 00:49:10 +0600

    Новое представление, новое url

--- lists/views.py
+++ lists/views.py
@@ -6,10 +6,6 @@ from lists.models import Item
 
 def home_page(request):
     ''' Домашняя страница '''
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/only-list-in-the-world')
-    items = Item.objects.all()
     return render(request, 'home.html')
 
 
@@ -17,3 +13,9 @@ def view_list(request):
     ''' представления списка '''
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    ''' новый список '''
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/only-list-in-the-world/')
