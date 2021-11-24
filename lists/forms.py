2bde988eb6dbc94debb328ad04eb52a71edff400
Balabi94 <orfejorfeev@gmail.com>
2021-11-25 02:35:08 +0600

    Новая форма для элементов списка

---
+++ lists/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.forms import widgets
+
+from lists.models import Item
+
+
+EMPTY_ITEM_ERROR = 'You cant have an empty list item'
+
+class ItemForm(forms.models.ModelForm):
+    ''' форма для элементов списка '''
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
+                })
+        }
+        error_messages = {
+                'text': {'required': EMPTY_ITEM_ERROR},
+                }
